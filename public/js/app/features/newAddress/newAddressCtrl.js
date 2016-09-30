367b6a06536a14e858e60ac722e2484a8c68ca9d
Blake Hagen <blakehagen@gmail.com>
2016-09-30 10:59:59 -0600

    address validation

--- public/js/app/features/newAddress/newAddressCtrl.js
+++ public/js/app/features/newAddress/newAddressCtrl.js
@@ -1,5 +1,20 @@
 angular.module('snailbox').controller('newAddressCtrl', function ($state, $location) {
   var newAddressCtrl = this;
 
+  newAddressCtrl.error = false;
+
+  newAddressCtrl.submitNewAddress = function (isValid) {
+    newAddressCtrl.error = false;
+    console.log('isValid', isValid);
+    if (!isValid) {
+      newAddressCtrl.error = true;
+      console.log('FORM NOT VALID');
+      return false;
+    } else {
+      console.log('newAddressCtrl.newAddressData', newAddressCtrl.newAddressData);
+      console.log('Form is Valid');
+    }
+  };
+
 
 });
