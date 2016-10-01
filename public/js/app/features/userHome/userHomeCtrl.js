49037dd392ba39f66ed1308335ad56118b0d3cfe
Blake Hagen <blakehagen@gmail.com>
2016-09-30 19:11:12 -0600

    redirect to enter address if no address present

--- public/js/app/features/userHome/userHomeCtrl.js
+++ public/js/app/features/userHome/userHomeCtrl.js
@@ -1,11 +1,14 @@
 'use strict';
 
-angular.module('snailbox').controller('userHomeCtrl', function ($stateParams, $state, userService) {
+angular.module('snailbox').controller('userHomeCtrl', function ($stateParams, $state, $location, userService) {
   var userHomeCtrl = this;
 
   userHomeCtrl.getUser = function () {
     userService.getUserById($stateParams.id)
       .then(function (user) {
+        if(!user.address.address1){
+          $location.path('/user/' + $stateParams.id + '/new');
+        }
       console.log('user', user);
     }).catch(function (error) {
       $state.go('login');
