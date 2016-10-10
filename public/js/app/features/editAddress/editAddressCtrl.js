87923af44d87c2dc43b8a66bc69fc7620b079efd
Blake Hagen <blakehagen@gmail.com>
2016-10-09 22:39:43 -0600

    edit address form

--- public/js/app/features/editAddress/editAddressCtrl.js
+++ public/js/app/features/editAddress/editAddressCtrl.js
@@ -4,5 +4,14 @@ angular.module('snailbox').controller('editAddressCtrl', function ($stateParams,
 
   var editAddressCtrl = this;
 
+  editAddressCtrl.getUser = function () {
+    userService.getUserById($stateParams.id).then(function (userData) {
+      editAddressCtrl.userAddress = userData.address;
+      console.log('editAddressCtrl.userAddress', editAddressCtrl.userAddress);
+    });
+  };
+
+  editAddressCtrl.getUser();
+
 
 });
