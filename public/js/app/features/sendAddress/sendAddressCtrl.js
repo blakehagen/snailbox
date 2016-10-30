90fa566ac9d32e6540e3510c00a8d4f86b4e3963
Blake Hagen <blakehagen@gmail.com>
2016-10-30 15:19:53 -0600

    selecting user to add and clearing input

--- public/js/app/features/sendAddress/sendAddressCtrl.js
+++ public/js/app/features/sendAddress/sendAddressCtrl.js
@@ -11,5 +11,10 @@ angular.module('snailbox')
 
     sendAddressCtrl.getAllUsers();
 
-
+    sendAddressCtrl.requestedUsers                = [];
+    sendAddressCtrl.addUserToRequests = function (selectedUser) {
+      sendAddressCtrl.requestedUsers.push(selectedUser);
+      $scope.$broadcast('angucomplete-alt:clearInput');
+      console.log('sendAddressCtrl.requestedUsers', sendAddressCtrl.requestedUsers);
+    };
   });
