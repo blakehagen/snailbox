07816a5ee203b76b9a2f736c108b290cc6ed6cb2
Blake Hagen <blakehagen@gmail.com>
2016-10-30 13:46:20 -0600

    new header and header icons

--- public/js/app/directives/userHeader/userHeader.js
+++ public/js/app/directives/userHeader/userHeader.js
@@ -4,10 +4,13 @@ angular.module('snailbox').directive('userHeader', function () {
     scope: {},
     templateUrl: './app/directives/userHeader/userHeaderTmpl.html',
     controllerAs: 'ctrl',
-    controller: function ($location, tokenService) {
+    controller: function ($location, $stateParams, tokenService) {
       this.logout = function () {
         tokenService.removeToken();
         $location.path('/');
+      };
+      this.userHome = function () {
+        $location.path('/user/' + $stateParams.id);
       };
     },
     bindToController: true
