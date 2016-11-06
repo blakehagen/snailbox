3012b04982e95dfcba5c3222034c1e3bb437695c
Blake Hagen <blakehagen@gmail.com>
2016-11-06 16:01:41 -0700

    check for connections and pending invites before adding user to request bucket

--- public/js/app/features/sendAddress/sendAddressCtrl.js
+++ public/js/app/features/sendAddress/sendAddressCtrl.js
@@ -2,14 +2,25 @@ angular.module('snailbox')
   .controller('sendAddressCtrl', function ($rootScope, $scope, $state, $stateParams, $location, userService, _) {
     var sendAddressCtrl = this;
 
+    // =============== //
     sendAddressCtrl.getAllUsers = function () {
       userService.getAllUsers().then(function (allUsers) {
         sendAddressCtrl.allUsers = allUsers;
-        console.log('sendAddressCtrl.allUsers', sendAddressCtrl.allUsers);
+        var currentUserIdx       = _.findIndex(sendAddressCtrl.allUsers, {_id: $stateParams.id});
+        var currentUser          = _.slice(sendAddressCtrl.allUsers, currentUserIdx)[0];
+        sendAddressCtrl.allUsers.splice(currentUserIdx, 1);
+        console.log('logged in user =>', currentUser);
+        sendAddressCtrl.connectionsAndRequests = _.concat(currentUser.connections, currentUser.pendingInvitationsSent, currentUser.pendingInvitationsRecieved);
+        _.each(sendAddressCtrl.connectionsAndRequests, function (id, index) {
+          if (!id) {
+            sendAddressCtrl.connectionsAndRequests.splice(index, 1);
+          }
+        });
       });
     };
 
     sendAddressCtrl.getAllUsers();
+    // =============== //
 
     sendAddressCtrl.requestedUsers    = [];
     sendAddressCtrl.addUserToRequests = function (selectedUser) {
@@ -19,20 +30,22 @@ angular.module('snailbox')
       if (_.findIndex(sendAddressCtrl.requestedUsers, {_id: selectedUser._id}) >= 0) {
         return false;
       }
+      if (_.includes(sendAddressCtrl.connectionsAndRequests, selectedUser._id)) {
+        return false;
+      }
+
       sendAddressCtrl.requestedUsers.push(selectedUser);
       $scope.$broadcast('angucomplete-alt:clearInput');
-      console.log('sendAddressCtrl.requestedUsers', sendAddressCtrl.requestedUsers);
     };
 
     sendAddressCtrl.removeUserFromRequests = function (userToRemove) {
       var removeItemIdx = _.findIndex(sendAddressCtrl.requestedUsers, {_id: userToRemove._id});
       sendAddressCtrl.requestedUsers.splice(removeItemIdx, 1);
-      console.log('sendAddressCtrl.requestedUsers', sendAddressCtrl.requestedUsers);
     };
 
     sendAddressCtrl.sendInvites = function () {
       var ids = [];
-      _.each(sendAddressCtrl.requestedUsers, function(invitation){
+      _.each(sendAddressCtrl.requestedUsers, function (invitation) {
         ids.push(invitation._id);
       });
       userService.sendInvites($stateParams.id, ids).then(function (response) {
