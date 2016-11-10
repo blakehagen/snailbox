c55623f6bda22fb46412160d007778d95fef7412
Blake Hagen <blakehagen@gmail.com>
2016-11-09 21:33:40 -0700

    building out review view, adding checkboxes, styles list of invites

--- public/js/app/features/review/reviewCtrl.js
+++ public/js/app/features/review/reviewCtrl.js
@@ -2,18 +2,30 @@ angular.module('snailbox')
   .controller('reviewCtrl', function ($stateParams, $location, userService, _) {
     var reviewCtrl = this;
 
+    // // // // //
     reviewCtrl.getUser = function () {
       userService.getConnections($stateParams.id)
         .then(function (user) {
           reviewCtrl.received = user.pendingInvitationsReceived;
+          _.each(reviewCtrl.received, function (user) {
+            user.selected = false;
+          });
           console.log('reviewCtrl.received', reviewCtrl.received);
         }).catch(function (error) {
         console.log('error', error);
       });
     };
 
-
     reviewCtrl.getUser();
+    // // // // //
+
+
+    reviewCtrl.selectUser = function (selected, invite, thisUser) {
+      invite.selected = !invite.selected;
+      console.log('invite', invite);
+      console.log('thisUser', thisUser);
+      console.log('selected', selected);
+    };
 
 
   });
